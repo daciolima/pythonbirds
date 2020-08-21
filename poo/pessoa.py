ffbcd7ea9b2dd5ba0809a3ca2622c60ad68f3445
Dácio Lima <daciolima@MacBook-Pro.local>
2020-08-21 13:06:27 -0300

    Code Criado atributo complexo filhos

--- poo/pessoa.py
+++ poo/pessoa.py
@@ -1,18 +1,23 @@
 class Pessoa:
     # Definição de atributo de instância ou de objeto são realizados no construtor do Objeto
-    def __init__(self, nome=None, idade=40):
+    def __init__(self, *filhos, nome=None, idade=40):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Dácio Lima')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    print(p.idade)
+    isaac = Pessoa(nome='Isaac', idade=2)
+    dacio = Pessoa(isaac, nome='Dácio Lima', idade=40)
+    print(Pessoa.cumprimentar(dacio))
+    print(id(dacio))
+    print(dacio.cumprimentar())
+    print(dacio.nome)
+    print(dacio.idade)
+    print(dacio.filhos)
+    for filho in dacio.filhos:
+        print(f'Nome: {filho.nome}, idade: {filho.idade}')
 
